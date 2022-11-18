43f24b6e6e81f76a80a00eb8f609e857c1b43fab
Gusiha <TUGARIN@TUGARIN>
2022-11-19 00:39:05 +0300

    Add project files.

---
+++ Task 10.3/NumberArray.cs
@@ -0,0 +1,65 @@
+﻿namespace Task_10._3
+{
+
+    // делегат из задания 2
+    delegate void SortDelegate(NumberArray numbers);
+
+    internal class NumberArray
+    {
+        // Свойство для получения кол-ва элементов в массиве (для удобства)
+        public int Amount { get; }
+
+        private int[] _array;
+
+        // Конструктор, в котором выделяется место под массив размера [amount] и заполняется рандомными числами от 0 до 100
+        public NumberArray(int amount)
+        {
+            Amount = amount;
+            Random rand = new();
+            _array = new int[amount];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = rand.Next() % 100;
+            }
+        }
+
+        public void Sort(SortDelegate sort)
+        {
+            // вызов делегата c проверкой на NULL (запускается функция сортировки)
+            sort?.Invoke(this);
+        }
+
+        // Индексатор
+        public int this[int i]
+        {
+            get
+            {
+                if (i < _array.Length)
+                    return _array[i];
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+            set
+            {
+                if (i < _array.Length)
+                    _array[i] = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        
+        public int[] Copy()
+        {
+            int[] array = new int[_array.Length];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                array[i] = _array[i];
+            }
+            return array;
+        }
+
+
+    }
+}
---
+++ Task 10.3/Program.cs
@@ -0,0 +1,59 @@
+﻿using Task_10._3;
+
+NumberArray numberArray = new(10);
+
+// Добавление в делегат функции BubbleSort
+SortDelegate delegates = BubbleSort;
+
+#region Демонстрация функционала
+
+// Копирование массива
+int[] newArr = numberArray.Copy();
+
+// Вывод изначального состояния
+Console.Write("Изначальный массив: ");
+for (int i = 0; i < numberArray.Amount; i++)
+{
+    Console.Write($"{numberArray[i]} ");
+}
+Console.WriteLine();
+
+// Сортировка через делегат 
+numberArray.Sort(delegates);
+
+Console.Write("Отсортированный массив: ");
+
+// Вывод после сортировки
+for (int i = 0; i < numberArray.Amount; i++)
+{
+    Console.Write($"{numberArray[i]} ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < newArr.Length; i++)
+{
+    Console.Write($"{newArr[i]} ");
+}
+#endregion
+
+// Сортировка пузырьком, которая идентична сигнатуре делегата - 1ое задание
+void BubbleSort(NumberArray numbers)
+{
+    for (int i = 0, temp = 0; i < numbers.Amount - 1; i++)
+    {
+        for (int j = 0; j < numbers.Amount - i - 1; j++)
+        {
+            if (numbers[j] > numbers[j + 1])
+            {
+                temp = numbers[j + 1];
+                numbers[j + 1] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+
+    }
+}
+
+
+
+
